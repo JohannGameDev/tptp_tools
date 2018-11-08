03d3d5cbe0cf60b6b9b5bb73e4412fde88ec1f92
Johann Strama <git@johannstrama.de>
2018-11-08 13:45:17 +0100

    almost finished, json string as arg and fof or thf formula and dynamic format

--- tptp_tools/tptp_to_latex.py
+++ tptp_tools/tptp_to_latex.py
@@ -23,24 +23,24 @@ In addition to that it creates the latex file.
 """
 class TptpListener(tListener):
 
-    def __init__(self,**kwargs):
+    def __init__(self,tptp_conf):
         # a tptp-file consists of one or more tptp-inputs (e.g. different axioms)
         self.latex_raw = []  # Contains all different tptp-inputs as raw latex
         dir_root = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
-        filename = dir_root.joinpath("tptp2tex","tptp_2_latex.json")
-        with open(filename) as f:  # tptp_2_latex: represent tptp in latex(e.g. ! is \forall)
-            data = json.load(f)
-        self.tptp_2_latex = data
+        print(tptp_conf)
+        tptp_conf = tptp_conf.replace('"','\\"')
+        print(tptp_conf)
+        data = json.loads(tptp_conf)
+        self.tptp_2_latex = data.get("replacementSymbols",None)
         filename = dir_root.joinpath("tptp2tex","template.tex")
         with open(filename, 'r') as latex_template:
-            data = latex_template.read()
-        self.latex_template = data
-#        self.styling = kwargs.get(0,None)
-        self.item_header = {"name":"","formula_role":"","fof_formula":""}# will be displayed as item header
-        self.styling = {"functor":"red","fof_formula":"blue"}
-        self.delete = {"name","formula_role"}
-        self.texcommands = {"red":"\\tptpred{", "blue":"\\tptpblue{"}# TODO: latex commands in json
-        self.styling_type = kwargs.get(1,None)
+            template_data = latex_template.read()
+        self.latex_template = template_data
+        self.styling = data.get("rulesStyling",None)
+        print(self.styling)
+        self.item_header = {"name":"","formula_role":""}# will be displayed as item header TODO: whats with plain formula
+        self.delete = data.get("deleteRules",None)
+        self.custom_parsing = data.get("customParsing",None)
         self.current_raw_latex = LatexRaw(self.styling, self.tptp_2_latex)  # Current raw latex code from tptp-input
         self.styled_latex = ""
 
@@ -52,20 +52,21 @@ class TptpListener(tListener):
         self.current_raw_latex.add_tex(self.match_style(ctx))  # This is the point where the Tree will be parsed into Latex
         self.current_raw_latex.add_name(self.item_header["name"])
         self.current_raw_latex.add_formula_role(self.item_header["formula_role"])
-        self.current_raw_latex.add_tptp_og_formula(self.item_header["fof_formula"])
+#        self.current_raw_latex.add_tptp_og_formula(self.item_header["fof_formula"])
         self.latex_raw.append(self.current_raw_latex) # collect all raw_latex to create a latex file from template
 
     """This function is to determine whether it should be parsed with default latex conversion or custom latex commands
     """
     def decide_match(self,ctx):
-        custom_tag = ["thf_variable_list"] # TODO:Custom tag seperate json
-        if tParser.ruleNames[ctx.getRuleIndex()] in custom_tag:
+
+        if tParser.ruleNames[ctx.getRuleIndex()] in self.custom_parsing:
             need = []
             for child in ctx.getChildren():
-                need.append(self.match_custom(child))
+                rules = self.custom_parsing.get(tParser.ruleNames[ctx.getRuleIndex()],None).get("rules",None)
+                need.append(self.match_custom(child,rules))
             custom_tex = ""
             for n in need:
-                custom_tex += " {variable}_{{{atomic_defined_word}{atomic_word}}} ".\
+                custom_tex += self.custom_parsing.get(tParser.ruleNames[ctx.getRuleIndex()],None).get("customTex",None) .\
                         format(variable=n.get("variable"),atomic_defined_word=n.get("atomic_defined_word"),atomic_word=n.get("atomic_word")) # TODO: This string in json
         else:
             custom_tex = self.match_style(ctx)
@@ -89,7 +90,7 @@ class TptpListener(tListener):
                         self.item_header[tParser.ruleNames[child.getRuleIndex()]] = child.getText() # fill in dictionary with predefined header
                     if tParser.ruleNames[child.getRuleIndex()] in self.styling: # Here are the rules that get a sorunding tex-tag
                         color = self.styling.get(tParser.ruleNames[child.getRuleIndex()]) # what if it has to be styled and is a custom_tag
-                        ret += self.texcommands.get(color) + self.decide_match(child) + "}"
+                        ret += color+"{" + self.decide_match(child) + "}"
                     else:
                         ret += self.decide_match(child)
                     if tParser.ruleNames[child.getRuleIndex()] in self.delete:
@@ -101,11 +102,11 @@ class TptpListener(tListener):
     # \lambda A_{ab}, B_{ab}, X_{a} . ( ( A \; X ) \lor (B \; X) )
         # for every thf_variable get variable(e.g. A) and thf_mapping type get text of children ignore > will be as indize
     """
-    def match_custom(self, ctx, custom_parse_data=None):
-        custom_parse = ["variable","atomic_defined_word","atomic_word"] # TODO: JSON
+    def match_custom(self, ctx,rules,custom_parse_data=None):
+        custom_parse = rules
 
         if custom_parse_data is None: # http://blog.thedigitalcatonline.com/blog/2015/02/11/default-arguments-in-python/ end of article
-            custom_parse_data = {"variable": "", "atomic_defined_word": "","atomic_word":""}# TODO: JSON
+             custom_parse_data ={ value: "" for value in custom_parse}
 
         if isinstance(ctx, TerminalNode):
             # Maybe for future development
@@ -118,9 +119,9 @@ class TptpListener(tListener):
                 else:
                     if tParser.ruleNames[child.getRuleIndex()] in custom_parse:
                         custom_parse_data[tParser.ruleNames[child.getRuleIndex()]] += self.getLatexCommand(child.getText()) # append indizes
-                        self.match_custom(child, custom_parse_data)
+                        self.match_custom(child,custom_parse, custom_parse_data)
                     else:
-                        self.match_custom(child, custom_parse_data)
+                        self.match_custom(child,custom_parse,custom_parse_data)
         return custom_parse_data
 
 
@@ -196,34 +197,19 @@ class LatexRaw:
 
         return raw_latex
 
-def create_latex_file(content_string):
-    lexer = tLexer(InputStream(content_string))
-    stream = CommonTokenStream(lexer)
-    parser = tParser(stream)
-    tree = parser.tptp_file()
-    printer = TptpListener() # <- Custom-Listener
-    walker = ParseTreeWalker()
-    walker.walk(printer, tree)
-    latex_raw = printer.create_latex_from_raw()
-    latex = printer.create_latex_file(latex_raw)
-    return latex
 
 def main(argv):
-    input = FileStream(argv[1])
-    lexer = tLexer(input)
+    lexer = tLexer(InputStream(argv[1]))
     stream = CommonTokenStream(lexer)
     parser = tParser(stream)
     tree = parser.tptp_file()
-    printer = TptpListener() # <- Custom-Listener
-
+    printer = TptpListener(argv[2])  # <- Custom-Listener
     walker = ParseTreeWalker()
-
     walker.walk(printer, tree)
-
-    latex_raw = printer.create_latex_from_raw()
-    latex = printer.create_latex_file(latex_raw)
-    print(latex)
-    #pyperclip.copy(latex) # for debugging
+    latex_raw = printer.create_latex_from_raw() # create latex body from all tptp-inputs(latex-raw-pbjects)
+    latex = printer.create_latex_file(latex_raw) # create latex file
+#    print(latex)
+    return latex
 
 if __name__ == '__main__':
     main(sys.argv)
